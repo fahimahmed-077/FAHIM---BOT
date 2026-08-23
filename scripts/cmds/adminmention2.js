38fb9aa7512dbe7025fb4490cfc3b8c751eb232f
FAHIM AHMED RAJ <fahimahmedraj207@gmail.com>
2026-08-24 04:32:59 +0600

    Update adminmention2.js

--- scripts/cmds/adminmention2.js
+++ scripts/cmds/adminmention2.js
@@ -36,7 +36,7 @@ module.exports = {
 
     const admins = [
       { uid: "", names: ["FAHIM VAI MBS"] },
-      { uid: "61591603332230", names: ["M'R FAHIM"] }
+      { uid: "61590371463064", names: ["M'R FAHIM"] }
     ];
 
     const senderID = String(event.senderID);
